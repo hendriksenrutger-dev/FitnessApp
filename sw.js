a62dc2f29e3cdcd263e7e42d09cf6ef0f432c778
hendriksenrutger-dev <hendriksen.rutger@gmail.com>
2026-04-18 11:10:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitapp-v33';
+const CACHE = 'fitapp-v34';
 const ASSETS = ['/FitnessApp/','/FitnessApp/index.html','/FitnessApp/manifest.json','/FitnessApp/icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
