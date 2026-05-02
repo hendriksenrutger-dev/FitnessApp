1d47da034bf3f9cceb23d1adb25b560f16c02f29
hendriksenrutger-dev <hendriksen.rutger@gmail.com>
2026-05-02 15:47:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-const CACHE = 'fitapp-v39';
+const CACHE = 'fitapp-v41';
 const ASSETS = ['/FitnessApp/','/FitnessApp/index.html','/FitnessApp/manifest.json','/FitnessApp/icon.svg'];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
-
-// Notificatie actie: gebruiker tikt "SET KLAAR" op vergrendelscherm
 self.addEventListener('notificationclick', e => {
   e.notification.close();
   e.waitUntil(
